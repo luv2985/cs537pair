25c6de8b3645395960378c00861f2e2db1e83fed
luv2985 <lu17vinc@gmail.com>
2024-03-26 14:37:40 -0500

    test 7 being funky, coming back later

--- p5/xv6-public/zombie.c
+++ p5/xv6-public/zombie.c
@@ -1,7 +1,6 @@
 #include "types.h"
 #include "user.h"
 
-
 void fn1(void* arg) {
   nice(10);
   for (int i = 0; i < 100000; i++) {
@@ -13,17 +12,33 @@ void fn1(void* arg) {
   exit();
 }
 
-int main() {
-  char* stack1 = (char*)malloc(4096);
-  clone(fn1, stack1 + 4096, 0);
-
+void fn2(void* arg) {
+  nice(-10);
   for (int i = 0; i < 100000; i++) {
     if (i % 1000 == 0) {
       sleep(0);
     }
   }
 
-  wait();
   exit();
 }
 
+int main() {
+	printf(1, "start\n");
+  char* stack1 = (char*)malloc(4096);
+  char* stack2 = (char*)malloc(4096);
+	printf(1, "mid\n");
+  clone(fn1, stack1	+ 4096, 0);
+  clone(fn2, stack2 + 4096, 0);
+	printf(1, "for?\n");
+
+  for (int i = 0; i < 100000; i++) {
+    if (i % 1000 == 0) {
+      sleep(0);
+    }
+  }
+	printf(1, "wait\n");
+  wait();
+  wait();
+  exit();
+}
